54309e2a128d19121b171c462f2bc3756fb72e26
Michael Groeneweg <75755543+michaelgroeneweg@users.noreply.github.com>
2022-08-08 15:15:54 +0200

    structure files for notifications.

---
+++ demo/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import Toast from '../../src'
+import App from './App.vue'
+
+createApp(App).use(Toast).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [vue(), AutoImport({ imports: ['vue'] })],
+  plugins: [vue(), AutoImport({ imports: ['vue'], dts: true })],
 })
